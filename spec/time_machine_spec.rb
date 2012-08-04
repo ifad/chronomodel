3bb766d9f952c6711fea6f6be75d0638285314af
Marcello Barnaba <vjt@openssl.it>
2012-08-04 18:47:41 +0200

    Fix tests breakage due to the last commit

--- spec/time_machine_spec.rb
+++ spec/time_machine_spec.rb
@@ -194,9 +194,9 @@ describe ChronoModel::TimeMachine do
     describe 'on records having a :belongs_to relationship' do
       subject { bar.history_timestamps }
 
-      describe 'returns timestamps of the record only' do
-        its(:size) { should == bar.ts.size }
-        it { should == timestamps_from.call(bar) }
+      describe 'returns timestamps of the record and its associations' do
+        its(:size) { should == foo.ts.size + bar.ts.size }
+        it { should == timestamps_from.call(foo, bar) }
       end
     end
   end
