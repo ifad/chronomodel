c58d758b09cf2ca3fe85fea7b0b4ee2da7764854
Marcello Barnaba <vjt@openssl.it>
2012-10-30 16:24:45 +0100

    Invoke schema creation in an initializer, or migrations will break

--- lib/chrono_model/railtie.rb
+++ lib/chrono_model/railtie.rb
@@ -1,17 +1,21 @@
 module ChronoModel
   class Railtie < ::Rails::Railtie
+    initializer :chrono_create_schemas do
+      ActiveRecord::Base.connection.chrono_create_schemas!
+    end
+
     rake_tasks do
 
-      namespace :db do
-        namespace :chrono do
-          task :create_schemas do
-            ActiveRecord::Base.connection.chrono_create_schemas!
-          end
-        end
-      end
+     namespace :db do
+       namespace :chrono do
+         task :create_schemas do
+           puts 'create schemas'
+           ActiveRecord::Base.connection.chrono_create_schemas!
+         end
+       end
+     end
 
       task 'db:schema:load' => 'db:chrono:create_schemas'
-      task 'db:migrate'     => 'db:chrono:create_schemas'
     end
 
     class SchemaDumper < ::ActiveRecord::SchemaDumper
