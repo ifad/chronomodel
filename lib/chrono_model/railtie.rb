987aceac4e71ccc94cc36c5b7a551cc93b8c29d8
Marcello Barnaba <vjt@openssl.it>
2013-01-16 18:06:51 +0100

    Remove the hacky schema dumper in favour of the SQL one

--- lib/chrono_model/railtie.rb
+++ lib/chrono_model/railtie.rb
@@ -17,30 +17,5 @@ module ChronoModel
 
       task 'db:schema:load' => 'db:chrono:create_schemas'
     end
-
-    class SchemaDumper < ::ActiveRecord::SchemaDumper
-      def tables(*)
-        super
-        @connection.send(:_on_temporal_schema) { super }
-      end
-
-      def indexes(table, stream)
-        super
-        if @connection.is_chrono?(table)
-          stream.rewind
-          t = stream.read.sub(':force => true', '\&, :temporal => true') # HACK
-          stream.seek(0)
-          stream.truncate(0)
-          stream.write(t)
-        end
-      end
-
-    end
-
-    # I'm getting (too) used to this (dirty) override scheme.
-    #
-    silence_warnings do
-      ::ActiveRecord::SchemaDumper = SchemaDumper
-    end
   end
 end
