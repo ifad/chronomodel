83f24cd98010950c8e513b3afbf98710595cb426
Marcello Barnaba <vjt@openssl.it>
2013-04-16 15:56:07 +0200

    Whitespace

--- lib/chrono_model/railtie.rb
+++ lib/chrono_model/railtie.rb
@@ -6,14 +6,14 @@ module ChronoModel
 
     rake_tasks do
 
-     namespace :db do
-       namespace :chrono do
-         task :create_schemas do
-           puts 'create schemas'
-           ActiveRecord::Base.connection.chrono_create_schemas!
-         end
-       end
-     end
+      namespace :db do
+        namespace :chrono do
+          task :create_schemas do
+            puts 'create schemas'
+            ActiveRecord::Base.connection.chrono_create_schemas!
+          end
+        end
+      end
 
       task 'db:schema:load' => 'db:chrono:create_schemas'
     end
