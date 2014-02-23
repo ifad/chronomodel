23f9b5c05f2e05a098677ec27677263394314530
Marcello Barnaba <vjt@openssl.it>
2014-02-24 00:42:33 +0100

    Bring README.sql up to date and better explain [skip ci]

--- README.sql
+++ README.sql
@@ -9,8 +9,9 @@ create schema history;  -- schema containing all history tables
 -- Current countries data - nothing special
 --
 create table temporal.countries (
-  id   serial primary key,
-  name varchar
+  id         serial primary key,
+  name       varchar,
+  updated_at timestamptz
 );
 
 -- Countries historical data.
@@ -43,8 +44,8 @@ create index country_recorded_at    on history.countries ( id, valid_to )
 create index country_instance_history on history.countries ( id, recorded_at )
 
 
--- The countries view, what the Rails' application ORM will actually CRUD on, and
--- the core of the temporal updates.
+-- The countries view, what the Rails' application ORM will actually CRUD on,
+-- and the entry point of the temporal triggers.
 --
 -- SELECT - return only current data
 --
@@ -63,7 +64,8 @@ create or replace function public.chronomodel_countries_insert() returns trigger
   end;
 $$ language plpgsql;
 
-create trigger chronomodel_insert instead of insert on public.countries
+create trigger chronomodel_insert
+  instead of insert on public.countries
   for each row execute procedure public.chronomodel_countries_insert();
 
 -- UPDATE - set the last history entry validity to now, save the current data
@@ -73,17 +75,38 @@ create trigger chronomodel_insert instead of insert on public.countries
 -- exists, update it with new data. This logic makes possible to "squash"
 -- together changes made in a transaction in a single history row.
 --
+-- If the update doesn't change the data, it is skipped and the trigger
+-- returns NULL.
+--
+-- By default, history is not recorded if only the updated_at field
+-- is changed.
+--
 create function chronomodel_countries_update() returns trigger as $$
   declare _now timestamp;
   declare _hid integer;
+  declare _old record;
+  declare _new record;
   begin
+  
+    if old is not distinct from new then
+      return null;
+    end if;
+
+    _old := row(old.name);
+    _new := row(new.name);
+
+    if _old is not distinct from new then
+      update only temporal.countries set ( name, updated_at ) = ( new.name, new.updated_at ) where id = old.id
+      return new;
+    end if;
+
     _now := timezone('utc', now());
     _hid := null;
 
     select hid into _hid from history.countries where id = old.id and lower(validity) = _now;
 
     if _hid is not null then
-      update history.countries set ( name ) = ( new.name ) where hid = _hid;
+      update history.countries set ( name, updated_at ) = ( new.name ) where hid = _hid;
     else
       update history.countries set validity = tsrange(lower(validity), _now)
       where id = old.id and upper_inf(validity);
@@ -97,11 +120,13 @@ create function chronomodel_countries_update() returns trigger as $$
   end;
 $$ language plpgsql;
 
-create trigger chronomodel_update instead of update on countries
+create trigger chronomodel_update
+  instead of update on temporal.countries
   for each row execute procedure chronomodel_countries_update();
 
 -- DELETE - save the current data in the history and eventually delete the
 -- data from the temporal table.
+--
 -- The first DELETE is required to remove history for records INSERTed and
 -- DELETEd in the same transaction.
 --
@@ -123,7 +148,8 @@ create or replace function chronomodel_countries_delete() returns trigger as $$
   end;
 $$ language plpgsql;
 
-create trigger chronomodel_delete instead of delete on countries
+create trigger chronomodel_delete
+  instead of delete on temporal.countries
   for each row execute procedure chronomodel_countries_delete();
 
 -- EOF
