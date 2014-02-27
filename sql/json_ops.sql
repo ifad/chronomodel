a690e8ebcce240a45185f025122f204de429b215
Marcello Barnaba <vjt@openssl.it>
2014-02-27 01:19:20 +0100

    Use a 32-bit FNV hash instead of coercing python's one

--- sql/json_ops.sql
+++ sql/json_ops.sql
@@ -6,8 +6,18 @@
 --
 CREATE OR REPLACE FUNCTION json_hash( a json ) RETURNS INTEGER AS $$
   import json
-  hsh = hash(json.dumps(json.loads(a), sort_keys=True, separators=(',', ':')))
-  return hsh & (2**32-1) - 2**31
+
+  # http://www.isthe.com/chongo/tech/comp/fnv/#FNV-1a
+  def fnv1a(str):
+    hval = 2166136261
+    for s in str:
+      hval = hval ^ ord(s)
+      hval = (hval * 16777619) % (2**32)
+    return hval
+
+  hsh = fnv1a(json.dumps(json.loads(a), sort_keys=True, separators=(',', ':')))
+  return hsh - 2**31
+
 $$ LANGUAGE plpythonu STRICT IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION json_eq( a json, b json ) RETURNS BOOLEAN AS $$
