95f3df620c7c7f950617b06016f9f6f265e9f424
Marcello Barnaba <vjt@openssl.it>
2014-03-12 20:57:46 +0100

    Remove the last useless initialization bit :yum:

--- lib/chrono_model/railtie.rb
+++ lib/chrono_model/railtie.rb
@@ -2,16 +2,8 @@ module ChronoModel
   class Railtie < ::Rails::Railtie
     ActiveRecord::Tasks::DatabaseTasks.register_task /chronomodel/, ActiveRecord::Tasks::PostgreSQLDatabaseTasks
 
-    setup = proc { ActiveRecord::Base.connection.chrono_setup! }
-
     rake_tasks do
       load 'chrono_model/schema_format.rake'
-
-      namespace :db do
-        namespace(:chrono) { task :model, &setup }
-      end
-
-      task 'db:schema:load' => 'db:chrono:model'
     end
   end
 end
