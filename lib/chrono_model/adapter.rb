ifad/chronomodel

commit e8a6ec325fa3f44ad5a784335e9666e76d44cd00

Reduce the size of the generated triggers, using NEW.* where possible and PG-only UPDATE ( fields ) = ( values ) syntax.

lib/chrono_model/adapter.rb
@@ -526,6 +526,7 @@ module ChronoModel
         pk      = primary_key(table)
         current = [TEMPORAL_SCHEMA, table].join('.')
         history = [HISTORY_SCHEMA,  table].join('.')
+        seq     = serial_sequence(current, pk)
 
         options ||= chrono_metadata_for(table)
 
@@ -548,9 +549,7 @@ module ChronoModel
         columns = columns(table).map {|c| quote_column_name(c.name)}
         columns.delete(quote_column_name(pk))
 
-        updates = columns.map {|c| "#{c} = new.#{c}"}.join(",\n")
-
-        fields, values = columns.join(', '), columns.map {|c| "new.#{c}"}.join(', ')
+        fields, values = columns.join(', '), columns.map {|c| "NEW.#{c}"}.join(', ')
 
         # Columns to be journaled. By default everything except updated_at (GH #7)
         #
@@ -571,14 +570,17 @@ module ChronoModel
 
         # INSERT - insert data both in the temporal table and in the history one.
         #
+        # The serial sequence is invoked manually for brevity, to be able to use
+        # VALUES ( NEW.* ) in the INSERT statement. Anyway, it is a requirement
+        # enforced above.
+        #
         execute <<-SQL
           CREATE OR REPLACE FUNCTION chronomodel_#{table}_insert() RETURNS TRIGGER AS $$
             BEGIN
-              INSERT INTO #{current} ( #{fields} ) VALUES ( #{values} )
-              RETURNING #{pk} INTO NEW.#{pk};
+              NEW.#{pk} := nextval('#{seq}');
 
-              INSERT INTO #{history} ( #{pk}, #{fields}, validity )
-              VALUES ( NEW.#{pk}, #{values}, tsrange(timezone('UTC', now()), NULL) );
+              INSERT INTO #{current} VALUES ( NEW.* );
+              INSERT INTO #{history} VALUES ( NEW.*, DEFAULT, tsrange(timezone('UTC', now()), NULL), DEFAULT );
 
               RETURN NEW;
             END;
@@ -614,7 +616,7 @@ module ChronoModel
               _new := row(#{journal.map {|c| "NEW.#{c}" }.join(', ')});
 
               IF _old IS NOT DISTINCT FROM _new THEN
-                UPDATE ONLY #{current} SET #{updates} WHERE #{pk} = OLD.#{pk};
+                UPDATE ONLY #{current} SET ( #{fields} ) = ( #{values} ) WHERE #{pk} = OLD.#{pk};
                 RETURN NEW;
               END IF;
 
@@ -624,16 +626,15 @@ module ChronoModel
               SELECT hid INTO _hid FROM #{history} WHERE #{pk} = OLD.#{pk} AND lower(validity) = _now;
 
               IF _hid IS NOT NULL THEN
-                UPDATE #{history} SET #{updates} WHERE hid = _hid;
+                UPDATE #{history} SET ( #{fields} ) = ( #{values} ) WHERE hid = _hid;
               ELSE
                 UPDATE #{history} SET validity = tsrange(lower(validity), _now)
                 WHERE #{pk} = OLD.#{pk} AND upper_inf(validity);
 
-                INSERT INTO #{history} ( #{pk}, #{fields}, validity )
-                VALUES ( OLD.#{pk}, #{values}, tsrange(_now, NULL) );
+                INSERT INTO #{history} VALUES ( NEW.*, DEFAULT, tsrange(_now, NULL), DEFAULT );
               END IF;
 
-              UPDATE ONLY #{current} SET #{updates} WHERE #{pk} = OLD.#{pk};
+              UPDATE ONLY #{current} SET ( #{fields} ) = ( #{values} ) WHERE #{pk} = OLD.#{pk};
 
               RETURN NEW;
             END;

commit 8ea02d5df813db6ea05381450ce8da9fd7dc4345

Partially revert e8a6ec3, as it fails with data structure changes
The triggers implementation introduced with e8a6ec3 were relying
on the history table colums order, as they were using NEW.*, then
the validity timestamps.

However, when new columns are added to the parent table, they end
up in the history table AFTER the validity timestamps, making the
triggers fail.

As currently on PG there is no way to alter a table columns order:

  http://wiki.postgresql.org/wiki/Alter_column_position

we are reverting to the previous behaviour, with explicit column
names in the triggers.

lib/chrono_model/adapter.rb
@@ -526,7 +526,6 @@ module ChronoModel
         pk      = primary_key(table)
         current = [TEMPORAL_SCHEMA, table].join('.')
         history = [HISTORY_SCHEMA,  table].join('.')
-        seq     = serial_sequence(current, pk)
 
         options ||= chrono_metadata_for(table)
 
@@ -570,17 +569,16 @@ module ChronoModel
 
         # INSERT - insert data both in the temporal table and in the history one.
         #
-        # The serial sequence is invoked manually for brevity, to be able to use
-        # VALUES ( NEW.* ) in the INSERT statement. Anyway, it is a requirement
-        # enforced above.
+        # The serial sequence is invoked manually for clarity.
         #
         execute <<-SQL
           CREATE OR REPLACE FUNCTION chronomodel_#{table}_insert() RETURNS TRIGGER AS $$
             BEGIN
-              NEW.#{pk} := nextval('#{seq}');
+              INSERT INTO #{current} ( #{fields} ) VALUES ( #{values} )
+              RETURNING #{pk} INTO NEW.#{pk};
 
-              INSERT INTO #{current} VALUES ( NEW.* );
-              INSERT INTO #{history} VALUES ( NEW.*, DEFAULT, tsrange(timezone('UTC', now()), NULL), DEFAULT );
+              INSERT INTO #{history} ( #{pk}, #{fields}, validity )
+              VALUES ( NEW.#{pk}, #{values}, tsrange(timezone('UTC', now()), NULL) );
 
               RETURN NEW;
             END;
@@ -631,7 +629,8 @@ module ChronoModel
                 UPDATE #{history} SET validity = tsrange(lower(validity), _now)
                 WHERE #{pk} = OLD.#{pk} AND upper_inf(validity);
 
-                INSERT INTO #{history} VALUES ( NEW.*, DEFAULT, tsrange(_now, NULL), DEFAULT );
+                INSERT INTO #{history} ( #{pk}, #{fields}, validity )
+                     VALUES ( OLD.#{pk}, #{values}, tsrange(_now, NULL) );
               END IF;
 
               UPDATE ONLY #{current} SET ( #{fields} ) = ( #{values} ) WHERE #{pk} = OLD.#{pk};