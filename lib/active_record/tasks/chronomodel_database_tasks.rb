0df58d340066915f62f302776254d450f8cc0711
Ilya Scharrenbroich <ilya.j.s@gmail.com>
2017-08-07 14:24:53 -0400

    Moving chronomodel rake task logic to DatabaseTasks class

---
+++ lib/active_record/tasks/chronomodel_database_tasks.rb
@@ -0,0 +1,46 @@
+module ActiveRecord
+  module Tasks
+    class ChronomodelDatabaseTasks < PostgreSQLDatabaseTasks
+
+      def structure_dump(*arguments)
+        original_schema_search_path = configuration['schema_search_path']
+
+        if original_schema_search_path.present?
+          configuration['schema_search_path'] = original_schema_search_path.split(',').concat([
+            ChronoModel::Adapter::TEMPORAL_SCHEMA,
+            ChronoModel::Adapter::HISTORY_SCHEMA
+          ]).join(',')
+        end
+
+        super(*arguments)
+
+        filename = arguments.first
+        sql = File.read(filename).gsub(/CREATE SCHEMA (?!IF NOT EXISTS)/, '\&IF NOT EXISTS ')
+        File.open(filename, 'w') { |file| file << sql }
+        remove_sql_header_comments(filename)
+
+        configuration['schema_search_path'] = original_schema_search_path
+      end
+
+      private
+
+      def remove_sql_header_comments(filename)
+        sql_comment_begin = '--'
+        removing_comments = true
+        tempfile = Tempfile.open("uncommented_structure.sql")
+        begin
+          File.foreach(filename) do |line|
+            unless removing_comments && (line.start_with?(sql_comment_begin) || line.blank?)
+              tempfile << line
+              removing_comments = false
+            end
+          end
+        ensure
+          tempfile.close
+        end
+        FileUtils.mv(tempfile.path, filename)
+      end
+
+    end
+  end
+end
