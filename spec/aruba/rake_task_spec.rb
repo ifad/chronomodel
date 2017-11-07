0ea3c329f2c970224931cf689e2614122193887f
Marcello Barnaba <vjt@openssl.it>
2017-11-07 23:58:01 +0100

    DRY rake spec a bit

--- spec/aruba/rake_task_spec.rb
+++ spec/aruba/rake_task_spec.rb
@@ -12,15 +12,22 @@ describe 'rake tasks', type: :aruba do
     let(:last_command) { action && last_command_started }
 
     context 'given a file db/structure.sql' do
-      let(:structure_sql) { File.read(File.expand_path('fixtures/empty_structure.sql', __dir__)) }
-      before { write_file('db/structure.sql', structure_sql) }
+      let(:structure_sql) { 'fixtures/empty_structure.sql' }
+      before { write_file('db/structure.sql', File.read(File.expand_path(structure_sql, __dir__))) }
+      before { write_file('config/database.yml', File.read(File.expand_path(database_yml, __dir__))) }
 
-      specify { expect(last_command).to be_successfully_executed }
+      subject { expect(last_command).to be_successfully_executed }
+
+      context 'with default username and password', issue: 55 do
+        let(:database_yml) { 'fixtures/database_with_default_username_and_password.yml' }
+
+        specify { subject }
+      end
 
       context 'without a specified username and password', issue: 55 do
-        let(:database_yml) { File.read(File.expand_path('fixtures/database_without_username_and_password.yml', __dir__)) }
-        before { write_file('config/database.yml', database_yml) }
-        specify { expect(last_command).to be_successfully_executed }
+        let(:database_yml) { 'fixtures/database_without_username_and_password.yml' }
+
+        specify { subject }
       end
     end
   end
