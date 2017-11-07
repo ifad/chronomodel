40e09510575a913e8e46d687a5d07e1647b2178f
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-11-07 22:11:54 +0100

    Remove postgres 9.6 specific settings
    We have 9.4 on Travis at the moment

--- spec/aruba/fixtures/empty_structure.sql
+++ spec/aruba/fixtures/empty_structure.sql
@@ -1,11 +1,9 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
