ebf6cd0d98602b89c3c0a28e2c5fabcda38cdf8b
Marcello Barnaba <vjt@openssl.it>
2017-11-08 01:19:37 +0100

    Simplify the migrations test

--- spec/aruba/migrations_spec.rb
+++ spec/aruba/migrations_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 
 describe 'database migrations' do
   context 'after a migration was generated' do
+    before { write_file('config/database.yml',
+      File.read(File.expand_path('fixtures/database_without_username_and_password.yml', __dir__))) }
+
     before { run_simple('bundle exec rails g migration CreateModels name:string') }
 
-    describe 'bundle exec rake db:migrate', type: :aruba do
+    describe 'bundle exec rake db:migrate', :announce_stdout, :announce_stderr, type: :aruba do
       let(:action) { run('bundle exec rake db:migrate') }
       let(:last_command) { action && last_command_started }
 
       specify { expect(last_command).to be_successfully_executed }
-      specify { expect(last_command).to have_output /CreateModels: migrated/ }
-
-      context 'without a specified username and password', issue: 55 do
-        let(:content) { File.read(File.expand_path('fixtures/database_without_username_and_password.yml', __dir__)) }
-        before { write_file('config/database.yml', content) }
-        specify { expect(last_command).to be_successfully_executed }
-        specify { expect(last_command).to have_output /CreateModels: migrated/ }
-      end
+      specify { expect(last_command).to have_output(/CreateModels: migrated/) }
     end
   end
 end
