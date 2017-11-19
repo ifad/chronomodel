0be6fb49bbb5aac86e2fbd0c76e702e431b5db96
Marcello Barnaba <vjt@openssl.it>
2017-11-19 22:18:20 +0100

    Make migrations Rails 5.0 compatible

--- spec/aruba/fixtures/migrations/56/20171115195229_add_temporal_extension_to_impressions.rb
+++ spec/aruba/fixtures/migrations/56/20171115195229_add_temporal_extension_to_impressions.rb
@@ -1,4 +1,4 @@
-class AddTemporalExtensionToImpressions < ActiveRecord::Migration[5.1]
+class AddTemporalExtensionToImpressions < ActiveRecord::Migration[5.0]
   def self.up
     enable_extension 'btree_gist' unless extension_enabled?('btree_gist')
     change_table :impressions, temporal: true, copy_data: true
