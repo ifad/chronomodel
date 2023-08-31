397eed285a8ed41fb924b0ad6032c0cacdd70bca
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-08-31 10:31:52 +0200

    Improve test app creation
    - Remove unneeded libraries
    
    Also fixes an issue when running specs against Rails 5.2 / Ruby 2.2
    because of a squiggly heredoc in active storage

--- spec/fixtures/railsapp/config/environments/development.rb
+++ spec/fixtures/railsapp/config/environments/development.rb
@@ -12,27 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true if Rails.version < '7.0'
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true if Rails.version < '7.0'
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
--- spec/fixtures/railsapp/config/initializers/assets.rb
+++
@@ -1,3 +0,0 @@
-## this file overrides the default initialization of assets.rb
-
-Rails.application.config.assets.version = "1.0" if Rails.version < '7.0'
